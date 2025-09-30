d0e53e8ca98dadfa9c4166e967ad1ea30fccde26
manojsankhla <82398766+manojsankhla@users.noreply.github.com>
2025-09-30 21:15:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-const fadeInElements = document.querySelectorAll('.about');
+// Setup and start animation!
 
-window.addEventListener('scroll', handlescroll);
-const handlescroll = () => {
-    fadeInElements.forEach((element) => {
-        element.classList.add('fade-in'); // Add fade-in class
-        if (element.getBoundingClientRect().top < window.innerHeight) {
-            element.classList.add('.show');
-        }
-    });
+  
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (!navbar) return;
+    if (window.scrollY > 100) {
+        navbar.classList.add('scrolled-nav');
+    } else {
+        navbar.classList.remove('scrolled-nav');
+    }
+});
 
-    fadeInElements.forEach((element) => {
-        if (element.getBoundingClientRect().top < window.innerHeight) {
-            element.classList.add('.show');
-        }
-    });
-}
-
-handlescroll(); // Call the function on page load
+  
